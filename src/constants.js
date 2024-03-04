5ed37dc13ff6b30a11e329a1589523ce8da16060
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2024-03-04 13:33:06 +0500

    Update backend URL

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-// export const backendUrl = "https://poker-hero-go-46fbd90a335c.herokuapp.com";
-export const backendUrl = "http://localhost:3000";
+export const backendUrl = "https://gift-go-6f06abcae993.herokuapp.com";
+// export const backendUrl = "http://localhost:3000";
